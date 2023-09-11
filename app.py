2494e2f6a09cf826d4aa602d250362dfcaf9fd8d
gkelley <griffin@dripos.com>
2023-09-10 22:30:00 -0500

    update response

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
@@ -9,11 +10,26 @@ def hello_world():
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Start our TwiML response
+    incoming_msg = request.values.get('Body', '').lower()
+
     resp = MessagingResponse()
-
-    # Add a message
-    resp.message("The Robots are coming! Head for the hills!")
-
+    msg = resp.message()
+    
+    responded = False
+    if 'quote' in incoming_msg:
+        # return a quote
+        r = requests.get('https://api.quotable.io/random')
+        if r.status_code == 200:
+            data = r.json()
+            quote = f'{data["content"]} ({data["author"]})'
+        else:
+            quote = 'I could not retrieve a quote at this time, sorry.'
+        msg.body(quote)
+        responded = True
+    if 'cat' in incoming_msg:
+        # return a cat pic
+        msg.media('https://cataas.com/cat')
+        responded = True
+    if not responded:
+        msg.body('I only know about famous quotes and cats, sorry!')
     return str(resp)
